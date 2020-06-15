c7570a9e8b4293ca5c6a952e000d0b176a20b1df
Kcils360 <dukes.gregoryd@gmail.com>
2020-06-15 14:25:38 -0700

    loggin works

---
+++ ContactsSync/ContactsSync/Graph/GraphConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace ContactsSync
+{
+    public static class GraphConstants
+    {
+        // Defines the permission scopes used by the app
+        public readonly static string[] Scopes =
+        {
+            "User.Read",
+            "MailboxSettings.Read",
+            "Calendars.ReadWrite",
+            "Contacts.ReadWrite"
+        };
+    }
+}
